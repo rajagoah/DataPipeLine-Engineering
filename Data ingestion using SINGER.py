21fb6770e8f5eec3f9f4fa2e151bab4b611d40c3
Aakarsh Rajagopalan <51793634+rajagoah@users.noreply.github.com>
2020-04-28 16:30:47 -0400

    updated code (#4)
    Updated code that contains me bits that I explore how to traverse through a tuple and a set.
    this is my first time traversing through them. 
    This is part of my data camp tutorial

--- Data ingestion using SINGER.py
+++ Data ingestion using SINGER.py
@@ -16,3 +16,15 @@ json = {"properties" :
 
 #the write schema method converts the above JSON into a schema
 sg.write_schema(stream_name="user_stream",schema=json, key_properties = ["id"])
+
+#exploring the data types of the variables declared above
+#note that since the below variable is a set data type, we must remember that we cant slice a set.
+#when we use the next() operator along with iter() operator on the users, the elements will be output
+#in random order since the set is not an ordered collection. so everytime we run the below code, the output will vary for the users variable
+print("users is: ", type(users))
+print("accessing all elements of set by simply calling the name of the variable", next(iter(users)))
+
+print("cols is: ", type(cols))
+print("accessing all element of tuple using list's index notation", cols[0:3])
+
+print("json is: ", type(json))
