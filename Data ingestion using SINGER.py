a6d61ebeacb72e6240cf981c1cc51683114ff593
Aakarsh Rajagopalan <51793634+rajagoah@users.noreply.github.com>
2020-04-29 09:52:17 -0400

    updated code

--- Data ingestion using SINGER.py
+++ Data ingestion using SINGER.py
@@ -20,7 +20,8 @@ sg.write_schema(stream_name="user_stream",schema=json, key_properties = ["id"])
 #exploring the data types of the variables declared above
 #note that since the below variable is a set data type, we must remember that we cant slice a set.
 #when we use the next() operator along with iter() operator on the users, the elements will be output
-#in random order since the set is not an ordered collection. so everytime we run the below code, the output will vary for the users variable
+#in random order since the set is not an ordered collection. so everytime we run the below code, the
+# output will vary for the users variables
 print("users is: ", type(users))
 print("accessing all elements of set by simply calling the name of the variable", next(iter(users)))
 
@@ -28,3 +29,25 @@ print("cols is: ", type(cols))
 print("accessing all element of tuple using list's index notation", cols[0:3])
 
 print("json is: ", type(json))
+
+#--
+#--
+
+# Complete the JSON schema
+schema = {'properties': {
+                    'items' : {
+                                'brand'         : {'type': 'string'},
+                                'model'         : {'type': 'string'},
+                                'price'         : {'type': 'number'},
+                                'currency'      : {'type': 'string'},
+                                'quantity'      : {'type': 'integer', 'minimum': 1},
+                                'date'          : {'type': 'string', 'format': 'date'},
+                                'countrycode'   : {'type': 'string', 'pattern': "^[A-Z]{2}$"},
+                                'store_name'    : {'type': 'string'}
+                                }
+                        }
+        }
+
+# Write the schema
+sg.write_schema(stream_name='products', schema=schema, key_properties=[])
+print('writing done')
